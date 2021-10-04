0d61a19d2f521b9c0ac98627b5c54052a81bdaf7
Daniel Rabottini <da_nkr@live.com>
2021-10-05 05:31:43 +0900

    updated the restaurants controller show with new review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,8 @@
 class ReviewsController < ApplicationController
-  def new
-    @restaurant = Restaurant.find(params[:restaurant_id])
-    @review = Review.new
-  end
 
   def create
-    @review = Review.new(review_params)
     @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
     @review.restaurant = @restaurant
     if @review.save
       redirect_to restaurant_path(@restaurant)
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,6 @@ class Review < ApplicationRecord
   RATING = (0..5)
   belongs_to :restaurant
 
-  validates :content, presence: true
+  validates :content, presence: true, length: { minimum: 10 }
   validates :rating, presence: true, numericality: { only_integer: true }, inclusion: { in: RATING }
 end
